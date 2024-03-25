cf3cf477c5f8f7af04f48e79fb0dca419a9c1b96
Tim-Hedlund <tim.hedlund@elev.ga.ntig.se>
2024-03-25 09:39:36 +0100

    Skapade ett system för att hantera filer lätt, Det kommer sen användas för att spara spelet och skapa karaktärer genom en textfil.

--- src/FileMaster.java
+++ src/FileMaster.java
@@ -1,8 +1,113 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class FileMaster {
+    File file;
 
-    FileMaster() {}
+    FileMaster(File file) {
+        this.file = file;
+    }
+
+    String returnLine (int lineNumber) { //returnerar värdet vid en line av en fil
+
+        try{
+            Scanner fileReader = new Scanner(this.file);
+
+            for (int i = 0; i < lineNumber - 1; i++) {
+                if (!fileReader.hasNextLine()) {
+                    System.out.println("Error, ");
+                    System.out.println("Last line is " + i);
+                    return null;
+                }
+
+                fileReader.nextLine();
+
+            }
+            String returnValue = fileReader.nextLine();
+            fileReader.close();
+
+            return (returnValue);
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Error,");
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+
+    void printParagraph(int paragraphNum) {
+
+        try{
+            Scanner fileReader = new Scanner(this.file);
+            int rowNum = getParagraphRow(paragraphNum);
+
+            skipLines(fileReader, rowNum);
+
+            while (true) {
+
+                String currentLine = fileReader.nextLine();
+
+                if (currentLine.length() != 0) {
+
+                    System.out.println(currentLine);
+
+                } else {
+                    break;
+                }
+
+            }
+            fileReader.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Error,");
+            e.printStackTrace();
+        }
+
+    }
+
+    int getParagraphRow(int paragraphNum) { // Paragraf 0 är första paragrafen. Returnerar vilken linje en paragraph (x) börjar på
+
+        try{
+            Scanner fileReader = new Scanner(this.file);
+            int rowNum = 0;
+
+            for (int i = 0; i < paragraphNum; i++) {
+
+                while (true) {
+                    rowNum ++;
+
+                    if (fileReader.nextLine().length() == 0) {
+                        break;
+                    }
+
+                }
+
+            }
+            fileReader.close();
+
+            return rowNum;
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Error,");
+            e.printStackTrace();
+        }
+
+        return -1;
 
 
 
+    }
+
+    void skipLines(Scanner scanner, int lines) {
+
+        for (int i = 0; i < lines; i++) {
+
+            scanner.nextLine();
+
+        }
+
+    }
 
 }
--- src/Game.java
+++ src/Game.java
@@ -1,8 +1,12 @@
 import Input.InputMaster;
 
+import java.io.File;
+import java.util.ArrayList;
+
 public class Game {
     InputMaster input = new InputMaster();
     Team team;
+    ArrayList<Character> allCharacters = new ArrayList<Character>();
 
     Game () {
 
@@ -13,9 +17,15 @@ public class Game {
 
     void startGame() {
 
+
+
+
         System.out.println("Welcome to \"Escape the motherland\"");
         System.out.println("Chose to create a character or use a default one");
         System.out.println("write 1 for custom or 2 for a default character");
+
+        File characters = new File("src/characters.txt");
+        FileMaster characterFileMaster = new FileMaster(characters);
 
         int choice = input.getInt();
 
